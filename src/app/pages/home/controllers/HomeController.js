06f84118c484b709e0853860e8591fe55b6e7244
heshamelmasry77 <heshamelmasry@outlook.com>
2017-08-20 21:05:13 +0200

    Getting everything together two

---
+++ src/app/pages/home/controllers/HomeController.js
@@ -0,0 +1,23 @@
+/**
+ * @author heshamelmasry
+ * created on 23.03.2017
+ */
+
+(function () {
+    'use strict';
+
+    angular.module('BlurAdmin.pages.home')
+        .controller('HomeController', HomeController);
+
+    /** @ngInject */
+    function HomeController() {
+
+        var vm = this;
+        vm.init = init;
+
+        function init() {
+
+
+        }
+    }
+})();
